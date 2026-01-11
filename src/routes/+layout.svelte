ada19efdb396aac8ebd97100c698c8b7102c675d
vdud <varundudeja96@gmail.com>
2026-01-11 19:56:25 +0530

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon_io/favicon.ico";
 
 	let { children } = $props();
 </script>
@@ -8,10 +8,20 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+	<link
+		rel="preconnect"
+		href="https://fonts.gstatic.com"
+		crossorigin="anonymous"
+	/>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
 	<title>Varun Dudeja | Web Developer</title>
-	<meta name="description" content="Varun Dudeja - Professional Web Developer crafting modern, fast, and beautiful web experiences." />
+	<meta
+		name="description"
+		content="Varun Dudeja - Professional Web Developer crafting modern, fast, and beautiful web experiences."
+	/>
 </svelte:head>
 
 <div class="min-h-screen">
